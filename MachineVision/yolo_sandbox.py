39dbd7037f558dc440a1257f8e24e16dfe2b3b92
Mate Michaletzky <mate.michaletzky@evosoft.com>
2024-09-10 17:08:18 +0200

    Prototype of button_collection filler

--- MachineVision/yolo_sandbox.py
+++ MachineVision/yolo_sandbox.py
@@ -45,94 +45,22 @@ class Button:
         return f"Button: rel pos to 0 (x,y)= {self.rel_from_0_x}, {self.rel_from_0_y}"
 
 button_collection = {
-    "0": Button(0, 0),
-    "1": Button(1, 0),
-    "2": Button(2, 0),
-    "3": Button(3, 0),
-    "4": Button(4, 0),
-    "5": Button(5, 0),
-    "6": Button(6, 0),
-    "7": Button(7, 0),
-    "8": Button(8, 0),
-    "9": Button(9, 0),
-    "oe": Button(10, 0),
-    "ue": Button(11, 0),
-    "oo": Button(12, 0),
-    "Backspace": Button(13, 0),
-    "Insert": Button(14, 0),
-    "Home": Button(15, 0),
-    "PageUp": Button(16, 0),
-    "NumLock": Button(17, 0),
-    "NumpadDivide": Button(18, 0),
-    "NumpadMultiply": Button(19, 0),
-    "NumpadSubtract": Button(20, 0),
-    "Tab": Button(0, 1),
-    "q": Button(1, 1),
-    "w": Button(2, 1),
-    "e": Button(3, 1),
-    "r": Button(4, 1),
-    "t": Button(5, 1),
-    "z": Button(6, 1),
-    "u": Button(7, 1),
-    "i": Button(8, 1),
-    "o": Button(9, 1),
-    "p": Button(10, 1),
-    "oee": Button(11, 1),
-    "uu": Button(12, 1),
-    "Enter": Button(13, 1),
-    "Delete": Button(14, 1),
-    "End": Button(15, 1),
-    "PageDown": Button(16, 1),
-    "Numpad7": Button(17, 1),
-    "Numpad8": Button(18, 1),
-    "Numpad9": Button(19, 1),
+    "0": Button(0, 0), "1": Button(1, 0), "2": Button(2, 0), "3": Button(3, 0), "4": Button(4, 0), "5": Button(5, 0),
+    "6": Button(6, 0), "7": Button(7, 0), "8": Button(8, 0), "9": Button(9, 0), "oe": Button(10, 0), "ue": Button(11, 0), "oo": Button(12, 0),
+    "Backspace": Button(13, 0), "Insert": Button(14, 0), "Home": Button(15, 0), "PageUp": Button(16, 0), "NumLock": Button(17, 0),
+    "NumpadDivide": Button(18, 0), "NumpadMultiply": Button(19, 0), "NumpadSubtract": Button(20, 0),
+    "Tab": Button(0, 1), "q": Button(1, 1), "w": Button(2, 1), "e": Button(3, 1), "r": Button(4, 1), "t": Button(5, 1), "z": Button(6, 1),
+    "u": Button(7, 1), "i": Button(8, 1), "o": Button(9, 1), "p": Button(10, 1), "oee": Button(11, 1), "uu": Button(12, 1), "Enter": Button(13, 1),
+    "Delete": Button(14, 1), "End": Button(15, 1), "PageDown": Button(16, 1), "Numpad7": Button(17, 1), "Numpad8": Button(18, 1), "Numpad9": Button(19, 1), 
     "NumpadAdd": Button(20, 1),
-    "CapsLock": Button(0, 2),
-    "a": Button(1, 2),
-    "s": Button(2, 2),
-    "d": Button(3, 2),
-    "f": Button(4, 2),
-    "g": Button(5, 2),
-    "h": Button(6, 2),
-    "j": Button(7, 2),
-    "k": Button(8, 2),
-    "l": Button(9, 2),
-    "ae": Button(10, 2),
-    "aa": Button(11, 2),
-    "uee": Button(13, 2),
-    "Numpad4": Button(14, 2),
-    "Numpad5": Button(15, 2),
-    "Numpad6": Button(16, 2),
-    "Shift": Button(0, 3),
-    "ii": Button(1, 3),
-    "y": Button(2, 3),
-    "x": Button(3, 3),
-    "c": Button(4, 3),
-    "v": Button(5, 3),
-    "b": Button(6, 3),
-    "n": Button(7, 3),
-    "m": Button(8, 3),
-    ",": Button(9, 3),
-    ".": Button(10, 3),
-    "-": Button(11, 3),
-    "ShiftR": Button(12, 3),
-    "ArrowUp": Button(13, 3),
-    "Numpad1": Button(14, 3),
-    "Numpad2": Button(15, 3),
-    "Numpad3": Button(16, 3),
-    "NumpadEnter": Button(17, 3),
-    "Control": Button(0, 4),
-    "Windows": Button(1, 4),
-    "Alt": Button(2, 4),
-    "Space": Button(3, 4),
-    "AltGr": Button(5, 4),
-    "fn": Button(6, 4),
-    "ControlR": Button(7, 4),
-    "ArrowDown": Button(8, 4),
-    "ArrowLeft": Button(9, 4),
-    "ArrowRight": Button(10, 4),
-    "Numpad0": Button(11, 4),
-    "NumpadDel": Button(12, 4)
+    "CapsLock": Button(0, 2), "a": Button(1, 2), "s": Button(2, 2), "d": Button(3, 2), "f": Button(4, 2), "g": Button(5, 2), "h": Button(6, 2),
+    "j": Button(7, 2), "k": Button(8, 2), "l": Button(9, 2), "ae": Button(10, 2), "aa": Button(11, 2), "uee": Button(13, 2), "Numpad4": Button(14, 2),
+    "Numpad5": Button(15, 2), "Numpad6": Button(16, 2),
+    "Shift": Button(0, 3), "ii": Button(1, 3), "y": Button(2, 3), "x": Button(3, 3), "c": Button(4, 3), "v": Button(5, 3), "b": Button(6, 3),
+    "n": Button(7, 3), "m": Button(8, 3), ",": Button(9, 3), ".": Button(10, 3), "-": Button(11, 3), "ShiftR": Button(12, 3), "ArrowUp": Button(13, 3),
+    "Numpad1": Button(14, 3), "Numpad2": Button(15, 3), "Numpad3": Button(16, 3), "NumpadEnter": Button(17, 3),
+    "Control": Button(0, 4), "Windows": Button(1, 4), "Alt": Button(2, 4), "Space": Button(3, 4), "AltGr": Button(5, 4), "fn": Button(6, 4), "ControlR": Button(7, 4),
+    "ArrowDown": Button(8, 4), "ArrowLeft": Button(9, 4), "ArrowRight": Button(10, 4), "Numpad0": Button(11, 4), "NumpadDel": Button(12, 4)
 }
 
 #print(button_collection)
@@ -297,7 +225,7 @@ for i in range( len(buttons) ):
     print(f"The midpoint of the {i}. button on the picture= {buttons[i][0]} relativ to button '0'= {buttons[i][1]}")
 
 
-print("---------- DETERMINING WHICH BUTTON IS WHICH MIDPOINT ----------")
+print("---------- DETERMINING THE ROWS OF BUTTONS ----------")
 
 # In one row, every button is 
 def determine_row(first_index_of_row_element, last_index_of_row_element, buttons, empty_target_list):
@@ -338,8 +266,13 @@ for i in range( len(rows) ):
         print(rows[i][j])
 
 
+print("---------- DETERMINING WHICH BUTTON IS WHICH POINT ----------")
 
 
+for index, (button_name, button_properties) in enumerate(button_collection.items()):
+    button_properties.pixels_from_0_x = rows[button_properties.rel_from_0_y][button_properties.rel_from_0_x][0][0]
+    button_properties.pixels_from_0_y = rows[button_properties.rel_from_0_y][button_properties.rel_from_0_x][0][1]
+    print(f"{button_name} is on pixels x= {button_properties.pixels_from_0_x}, y= {button_properties.pixels_from_0_y}")
 
 
 
