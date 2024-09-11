5061d24edf846c272d6bbdb479c4e07c989d7bbb
Mate Michaletzky <mate.michaletzky@evosoft.com>
2024-09-11 08:17:54 +0200

    First working button detector

--- MachineVision/yolo_sandbox.py
+++ MachineVision/yolo_sandbox.py
@@ -45,22 +45,27 @@ class Button:
         return f"Button: rel pos to 0 (x,y)= {self.rel_from_0_x}, {self.rel_from_0_y}"
 
 button_collection = {
+    # First row
     "0": Button(0, 0), "1": Button(1, 0), "2": Button(2, 0), "3": Button(3, 0), "4": Button(4, 0), "5": Button(5, 0),
     "6": Button(6, 0), "7": Button(7, 0), "8": Button(8, 0), "9": Button(9, 0), "oe": Button(10, 0), "ue": Button(11, 0), "oo": Button(12, 0),
     "Backspace": Button(13, 0), "Insert": Button(14, 0), "Home": Button(15, 0), "PageUp": Button(16, 0), "NumLock": Button(17, 0),
     "NumpadDivide": Button(18, 0), "NumpadMultiply": Button(19, 0), "NumpadSubtract": Button(20, 0),
+    # Second row
     "Tab": Button(0, 1), "q": Button(1, 1), "w": Button(2, 1), "e": Button(3, 1), "r": Button(4, 1), "t": Button(5, 1), "z": Button(6, 1),
     "u": Button(7, 1), "i": Button(8, 1), "o": Button(9, 1), "p": Button(10, 1), "oee": Button(11, 1), "uu": Button(12, 1), "Enter": Button(13, 1),
     "Delete": Button(14, 1), "End": Button(15, 1), "PageDown": Button(16, 1), "Numpad7": Button(17, 1), "Numpad8": Button(18, 1), "Numpad9": Button(19, 1), 
     "NumpadAdd": Button(20, 1),
+    # Third row
     "CapsLock": Button(0, 2), "a": Button(1, 2), "s": Button(2, 2), "d": Button(3, 2), "f": Button(4, 2), "g": Button(5, 2), "h": Button(6, 2),
-    "j": Button(7, 2), "k": Button(8, 2), "l": Button(9, 2), "ae": Button(10, 2), "aa": Button(11, 2), "uee": Button(13, 2), "Numpad4": Button(14, 2),
-    "Numpad5": Button(15, 2), "Numpad6": Button(16, 2),
+    "j": Button(7, 2), "k": Button(8, 2), "l": Button(9, 2), "ae": Button(10, 2), "aa": Button(11, 2), "uee": Button(12, 2), "Numpad4": Button(13, 2),
+    "Numpad5": Button(14, 2), "Numpad6": Button(15, 2),
+    # Fourth row
     "Shift": Button(0, 3), "ii": Button(1, 3), "y": Button(2, 3), "x": Button(3, 3), "c": Button(4, 3), "v": Button(5, 3), "b": Button(6, 3),
     "n": Button(7, 3), "m": Button(8, 3), ",": Button(9, 3), ".": Button(10, 3), "-": Button(11, 3), "ShiftR": Button(12, 3), "ArrowUp": Button(13, 3),
     "Numpad1": Button(14, 3), "Numpad2": Button(15, 3), "Numpad3": Button(16, 3), "NumpadEnter": Button(17, 3),
-    "Control": Button(0, 4), "Windows": Button(1, 4), "Alt": Button(2, 4), "Space": Button(3, 4), "AltGr": Button(5, 4), "fn": Button(6, 4), "ControlR": Button(7, 4),
-    "ArrowDown": Button(8, 4), "ArrowLeft": Button(9, 4), "ArrowRight": Button(10, 4), "Numpad0": Button(11, 4), "NumpadDel": Button(12, 4)
+    # Fifth row
+    "Control": Button(0, 4), "Windows": Button(1, 4), "Alt": Button(2, 4), "Space": Button(3, 4), "AltGr": Button(4, 4), "fn": Button(5, 4), "ControlR": Button(6, 4),
+    "ArrowDown": Button(7, 4), "ArrowLeft": Button(8, 4), "ArrowRight": Button(9, 4), "Numpad0": Button(10, 4), "NumpadDel": Button(11, 4)
 }
 
 #print(button_collection)
@@ -254,10 +259,15 @@ rows = []
 for i in range(5):
     rows.append(list())
 
+# 21 elements in the first row
 determine_row(0, 20, buttons, rows[0])
+# 21 elements in the second row
 determine_row(21, 41, buttons, rows[1])
+# 16 elements in the third row
 determine_row(42, 57, buttons, rows[2])
+# 18 elements in the fourth row
 determine_row(58, 75, buttons, rows[3])
+# 12 elements in the fifth row
 determine_row(76, 87, buttons, rows[4])
 
 for i in range( len(rows) ):
